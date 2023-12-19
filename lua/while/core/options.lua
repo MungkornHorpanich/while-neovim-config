be107bfa1e34ad6fe84d5bc671bd6e7c6e1de5be
M <MungkornHorpanich@gmail.com>
2023-12-19 16:29:25 +0700

    Finishing everthing fr

--- lua/while/core/options.lua
+++ lua/while/core/options.lua
@@ -16,6 +16,9 @@ opt.wrap = false -- disable line wrapping
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
 
+-- cursor line
+opt.cursorline = true -- highlight the current cursor line
+
 -- appearance
 
 -- turn on termguicolors for nightfly colorscheme to work
