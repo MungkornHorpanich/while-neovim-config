781cc61e7f7ed3bc78f24b089c7d0bef67623ad8
M <MungkornHorpanich@gmail.com>
2023-12-19 15:45:34 +0700

    Fixing the lazy.lua to use with lsp

--- lua/while/lazy.lua
+++ lua/while/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("while.plugins", {
+require("lazy").setup({{import = "while.plugins"}, {import = "while.plugins.lsp"}}, {
   install = {
     colorscheme = { "kanagawa-dragon" },
   },
