917259351c670ceee49a51663c3ba76ba4eaad67
M <MungkornHorpanich@gmail.com>
2024-04-25 13:01:17 +0700

    Change the position of the diff for git

--- lua/while/plugins/lualine.lua
+++ lua/while/plugins/lualine.lua
@@ -12,11 +12,11 @@ return {
       },
       sections = {
         lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff'},
+        lualine_b = {'branch'},
         lualine_c = {'buffers'},
-        lualine_x = {'diagnostics', 'filetype'},
+        lualine_x = {'diagnostics', 'filetype', 'diff'},
         lualine_y = {'progress'},
-        lualine_z = {'location'}
+        lualine_z = {'location'},
       },
     })
   end,
