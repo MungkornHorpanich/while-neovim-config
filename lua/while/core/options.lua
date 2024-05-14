fc12e612134b629149efdc12e32aaac72e8e3490
M <MungkornHorpanich@gmail.com>
2024-05-14 14:08:45 +0700

    Idk

--- lua/while/core/options.lua
+++ lua/while/core/options.lua
@@ -2,10 +2,11 @@ local opt = vim.opt -- for conciseness
 
 -- line numbers
 opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+opt.relativenumber = true
 
 -- tabs & indentation
-opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.tabstop = 4 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 4 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
