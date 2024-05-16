d9b45ab9a968cbb439e85a6cd4f3201d3fb2c9c6
M <MungkornHorpanich@gmail.com>
2024-05-16 23:09:19 +0700

    Fixing the colorscheme

--- lua/while/plugins/colorscheme.lua
+++ lua/while/plugins/colorscheme.lua
@@ -4,24 +4,7 @@ return {
 		priority = 1000,
 		config = true,
 		opts = function()
-			return {
-				terminal_colors = true, -- add neovim terminal colors
-				undercurl = true,
-				underline = true,
-				bold = true,
-				strikethrough = true,
-				invert_selection = false,
-				invert_signs = false,
-				invert_tabline = false,
-				invert_intend_guides = false,
-				inverse = true, -- invert background for search, diffs, statuslines and errors
-				contrast = "", -- can be "hard", "soft" or empty string
-				palette_overrides = {},
-				overrides = {},
-				dim_inactive = false,
-				transparent_mode = true,
-			},
-				vim.cmd.colorschem("gruvbox")
+			vim.cmd("colorscheme gruvbox")
 		end,
 	},
 }
