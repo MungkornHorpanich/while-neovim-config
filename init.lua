f3ee7cb4ec5a552f748e9d3293f4d8b637ec6b92
M <MungkornHorpanich@gmail.com>
2024-06-08 19:06:35 +0700

    Changing the theme to rosepine

--- init.lua
+++ init.lua
@@ -3,3 +3,5 @@ require("while.lazy")
 
 vim.cmd("hi NvimTreeNormalNC guibg=NONE")
 vim.cmd("hi NvimTreeNormal guibg=NONE")
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
--- lua/while/plugins/colorscheme.lua
+++ lua/while/plugins/colorscheme.lua
@@ -1,10 +1,18 @@
 return {
 	{
-		"ellisonleao/gruvbox.nvim",
-		priority = 1000,
-		config = true,
-		opts = function()
-			vim.cmd("colorscheme gruvbox")
+
+		"rose-pine/neovim",
+
+		name = "rose-pine",
+
+		config = function()
+			require("rose-pine").setup({
+				disable_background = true,
+				disable_nc_background = true,
+				disable_float_background = true,
+			})
+
+			vim.cmd.colorscheme("rose-pine-moon")
 		end,
 	},
 }
