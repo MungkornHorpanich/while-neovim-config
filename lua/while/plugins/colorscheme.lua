97465fad79362a16dbeaa2d5645baae06550ac79
M <MungkornHorpanich@gmail.com>
2024-06-09 07:21:06 +0700

    Changing the theme to melange

--- lua/while/plugins/colorscheme.lua
+++ lua/while/plugins/colorscheme.lua
@@ -1,18 +1,6 @@
 return {
-	{
-
-		"rose-pine/neovim",
-
-		name = "rose-pine",
-
-		config = function()
-			require("rose-pine").setup({
-				disable_background = true,
-				disable_nc_background = true,
-				disable_float_background = true,
-			})
-
-			vim.cmd.colorscheme("rose-pine-moon")
-		end,
-	},
+	"savq/melange-nvim",
+	config = function()
+		vim.cmd("colorscheme melange")
+	end,
 }
