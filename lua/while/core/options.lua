773053cca8d941268641ae94fc2ee880e2f96c4e
M <MungkornHorpanich@gmail.com>
2024-06-16 16:27:14 +0700

    Fixing the bar

--- lua/while/core/options.lua
+++ lua/while/core/options.lua
@@ -26,7 +26,7 @@ opt.smartcase = true -- if you include mixed case in your search, assumes you wa
 -- (have to use iterm2 or any other true color terminal)
 opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-opt.signcolumn = "no" -- show sign column so that text doesn't shift
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
