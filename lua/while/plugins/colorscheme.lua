7ac6fd2da531c9869d80fc2e7aec16ea30a99d99
M <MungkornHorpanich@gmail.com>
2024-06-16 09:51:10 +0700

    Changing the theme to tokyonight and fixing some bug

--- lua/while/plugins/colorscheme.lua
+++ lua/while/plugins/colorscheme.lua
@@ -1,6 +1,13 @@
 return {
-	"savq/melange-nvim",
-	config = function()
-		vim.cmd("colorscheme melange")
-	end,
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			transparent = true,
+		},
+		config = function()
+			vim.cmd("colorscheme tokyonight-storm")
+		end,
+	},
 }
