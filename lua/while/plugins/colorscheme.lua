aa7cc86b80333efc886ac0595bb252fd0dfb5285
M <MungkornHorpanich@gmail.com>
2024-12-03 13:31:38 +0700

    idk

--- lua/while/plugins/colorscheme.lua
+++ lua/while/plugins/colorscheme.lua
@@ -3,9 +3,10 @@ return {
 		"folke/tokyonight.nvim",
 		config = function()
 			require("tokyonight").setup({
-				style = "storm", -- Options: "night", "storm", "day"
+				style = "night", -- Options: "night", "storm", "day"
 				transparent = false, -- Set to true for a transparent background
 				terminal_colors = true,
+				vim.cmd("colorscheme tokyonight"),
 			})
 		end,
 	},
