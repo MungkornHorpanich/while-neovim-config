093009d48723e3d1cf312e61360ad62fcb522d8f
M <MungkornHorpanich@gmail.com>
2024-12-16 09:46:52 +0700

    Changing to tokyonight

--- lua/while/plugins/colorscheme.lua
+++ lua/while/plugins/colorscheme.lua
@@ -1,10 +1,24 @@
 return {
 	{
-		"craftzdog/solarized-osaka.nvim",
+		"folke/tokyonight.nvim",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.cmd("colorscheme solarized-osaka")
+			require("tokyonight").setup({
+				style = "moon", -- Options: "storm", "moon", "night", "day"
+				transparent = true, -- Enable transparency
+				terminal_colors = true, -- Apply theme to terminal colors
+				styles = {
+					sidebars = "transparent", -- Style for sidebars (e.g., NvimTree, Telescope)
+					floats = "transparent", -- Style for floating windows
+				},
+				on_highlights = function(highlights, colors)
+					highlights.NormalFloat = { bg = "none" }
+					highlights.FloatBorder = { bg = "none" }
+				end,
+			})
+
+			vim.cmd("colorscheme tokyonight-storm")
 		end,
 	},
 }
